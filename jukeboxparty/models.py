14d8b9988af60cee7c8a7de2fcc097c98cb3c9a3
CodeGit <matloob@gmail.com>
2022-07-04 09:49:13 +0100

    Feature: Initialised Django project and added preliminary models

---
+++ jukeboxparty/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+class AudioFile(models.Model):
+    path = models.CharField(max_length=2048, primary_key=True)
+    filename = models.CharField(max_length=1024)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    
+class Musician(models.Model):
+    name = models.CharField(max_length=255)
+    sort_name = models.CharField(max_length=255)
+
+class Alias(models.Model):
+    name = models.ForeignKey(to=Musician, related_name='alias_name', on_delete=models.CASCADE)
+    alias = models.ForeignKey(to=Musician, on_delete=models.CASCADE)
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    release_date = models.DateField()
+    musicians = models.ManyToManyField(to=Musician)
+
+class Music(models.Model):
+    name = models.CharField(max_length=255)
+    sort_name = models.CharField(max_length=255)
+    album = models.ForeignKey(to=Album, on_delete=models.CASCADE)
+    audiofile = models.ForeignKey(to=AudioFile, on_delete=models.CASCADE)
+    release_date = models.DateField()
+    genre = models.ForeignKey(to=Genre, on_delete=models.CASCADE)
+    musicians = models.ManyToManyField(to=Musician)
+    tags = models.ManyToManyField(to=Tag)
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+class Rating(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    music = models.ForeignKey(to=Music, on_delete=models.CASCADE)
+    value = models.PositiveIntegerField()
---
+++ tests/test_import.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+from jukeboxparty.models import AudioFile
+
+ 
